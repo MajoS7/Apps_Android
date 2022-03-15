049fbb989cc00fb0b27b334754c89799249796b2
Majos7 <majos2764@gmail.com>
2022-03-15 14:57:07 -0500

    inicio de la aplicacion de notas

---
+++ ProyectoNotas/app/src/main/java/com/majos7/proyectonotas/Splash_Screen.kt
@@ -0,0 +1,13 @@
+package com.majos7.proyectonotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+    }
+}
